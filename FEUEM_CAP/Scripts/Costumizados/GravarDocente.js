9ea26d6654a2927e24d8cbd3fe1d517b5d7c14f0
Luis Nuaila Jr. <luisnuailajr@gmail.com>
2019-03-04 13:00:27 +0200

    Verificacao

---
+++ FEUEM_CAP/Scripts/Costumizados/GravarDocente.js
@@ -0,0 +1,33 @@
+﻿var btnAcaoDocente = $("#gravarDocente");
+
+var formularioDocente = $("#formCrudDocente");
+
+btnAcaoDocente.on("click", submeterDocente);
+
+function submeterDocente() {
+
+    if (formularioDocente.valid()) {
+
+        var urlDocente = formularioDocente.prop("action");
+        var metodoDocente = formularioDocente.prop("method");
+        var dadosFormularioDocente = formularioDocente.serialize();
+
+        $.ajax({
+            url: urlDocente,
+            type: metodoDocente,
+            data: dadosFormularioDocente,
+            success: tratarRetornoDocente
+        });
+    }
+}
+
+function tratarRetornoDocente(resultadoServidor) {
+
+    if (resultadoServidor.resultado) {
+        toastr["success"](resultadoServidor.mensagem);
+        window.location.href("@Url.Action('Index'','Docente')");
+
+    } else {
+        toastr["error"](resultadoServidor.mensagem);
+    }
+}
